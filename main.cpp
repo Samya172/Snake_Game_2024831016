98a8d78784a624d1756b72841bce8e162dfe232c
Swapneel Bagchi Samya <samyabagchi172@gmail.com>
2026-05-27 13:20:58 +0600

    Header FIle Set Up

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<SDL3/SDL.h>
+#include<cstdlib>
+#include<ctime>
+#include<cstdio>
+
+#define width 20
+#define height 15
+#define cell 32
+
+#define win_width (width * cell)
+#define win_height (height * cell)
+
+#define max_snake 100
